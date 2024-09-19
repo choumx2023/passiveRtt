6135cb391d1f775b0b88e212526f3f282e11b6a6
choumx2023 <choumx23@mails.tsinghua.edu.cn>
2024-09-19 20:13:50 +0800

    20240919_2013_mac

--- src/temp.py
+++ src/temp.py
@@ -1,32 +1,86 @@
-class CompressedIPNode:
-    def __init__(self, network):
-        self.network = network
-        self.children = {}
-        self.parent = None  # 指向父节点
-        self.stats = defaultdict(int)  # 统计信息
 
-    def record_activity(self, activity_type, count=1):
-        # 在当前节点记录活动
-        self.stats[activity_type] += count
-        # 递归更新父节点的统计信息
-        if self.parent:
-            self.parent.record_activity(activity_type, count)
-def record_activity(self, ip, activity_type, count=1, timestamp=None):
-        trie = self.ipv4_trie if ipaddress.ip_address(ip).version == 4 else self.ipv6_trie
-        node = trie.find_node(ip)
-        if node:
-            node.record_activity(activity_type, count, timestamp)
+def defalute_state():
+    return {
+        'forward_range' : [-1, -1],
+        'backward_range': [-1, -1],
+        'forward_sack_range' : [-1, -1],
+        'backward_sack_range': [-1, -1],
+        'time_series': []
+        # 重传：
+        # 我发对面没发：我确认的不变，但是我放最远的发
+        # 对面发我没发：我的序号不变，但是我确认对面的
+        # 我发对面发： 自己控制的同时更新，并且不超过对面的最大值
+        # 丢包：非常规的确认 比如出现了我确认了比对面最远的还遥远的数据
+    }
+
+
+MAX_SEQ = 0xFFFFFFFF  # 4294967295, TCP最大序列号
+
+def seq_compare(seq1, seq2):
+    """
+    比较两个序列号，考虑回环。
+    如果 seq1 在 seq2 之后，返回 True。
+    """
+    if seq2 == -1: # 未初始化
+        return True
+    return (seq1 - seq2) % (MAX_SEQ + 1) < (MAX_SEQ + 1) // 2
+
+
+def calculate_tcp_states(packet : dict):
+    check = 0
+    states = defalute_state()
+    seq = packet['seq']
+    ack = packet['ack']
+    len = packet['len']
+    is_syn = packet['syn'] == 1
+    is_ack = packet['ack'] == 1
+    furthest = seq + len - 1 + is_syn
+    nearest = ack - 1
+    judge = 0
+    if packet['direction'] == 'forward':
+        if seq_compare(furthest, states['forward_range'][0]):
+            states['forward_range'][0] = furthest
+            judge += 1
+        if seq_compare(nearest, states['backward_range'][0]):
+            states['backward_range'][0] = nearest
+            judge += 2
+        if judge == 0: # 没有更新 需要判定是重传还是心跳
+            if is_syn and is_ack:
+                states['time_series'].append('syn-ack')
+            elif is_syn:
+                states['time_series'].append('syn')
+            elif len == 0:
+                states['time_series'].append('forward heartbeat')
+            else:
+                states['time_series'].append('forward retransmission')
+        elif judge == 1: # 只更新了自己的further range
+            states['time_series'].append('forward back to back')
+        elif judge == 2: # 只更新了对面的nearest range
+            states['time_series'].append('forward ack')
+        else: # 同时更新了两个range
+            states['time_series'].append('forward normal')
+    
+    else:
+        if seq_compare(furthest, states['backward_range'][0]):
+            states['backward_range'][0] = furthest
+            judge += 1
+        if seq_compare(nearest, states['forward_range'][0]):
+            states['forward_range'][0] = nearest
+            judge += 2
+        if judge == 0:
+            if is_syn and is_ack:
+                states['time_series'].append('syn-ack')
+            elif is_syn:
+                states['time_series'].append('syn')
+            elif len == 0:
+                states['time_series'].append('backward heartbeat')
+            else:
+                states['time_series'].append('backward retransmission')
+        elif judge == 1:
+            states['time_series'].append('backward back to back')
+        elif judge == 2:
+            states['time_series'].append('backward ack')
+        else:
+            states['time_series'].append('backward normal')
+        
             
-def record_activity(self, activity_type, count=1, timestamp=None):
-        # 添加计数和时间戳
-        self.stats[activity_type]['count'] += count
-        if timestamp:
-            self.stats[activity_type]['timestamps'].append(timestamp)
-        # 递归更新父节点
-        if self.parent:
-            self.parent.record_activity(activity_type, count, timestamp)
-def query_activity(self, activity_type):
-        # 获取活动的计数和所有相关时间戳
-        count = self.stats[activity_type]['count']
-        timestamps = self.stats[activity_type]['timestamps']
-        return count, timestamps
